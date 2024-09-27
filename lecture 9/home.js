bc5b60d23dcb5139b620aa2f81bdbffe86e312ee
pratham-107 <spratham845@gmail.com>
2024-09-27 19:15:12 +0530

    code update

---
+++ lecture 9/home.js
@@ -0,0 +1,6 @@
+let user = 'hellllllo'
+
+function sum(a,b){
+    console.log(a+b)
+}
+export {user,sum}
---
+++ lecture 9/script.js
@@ -0,0 +1,45 @@
+// $('h2').css({
+//     color: "red",
+//     backgroundColor: 'green'
+// })
+// console.log($('h2').text());
+
+
+// console.log($('a').attr('href','https://flipkart.com'));
+
+// $('#btn1').on('click',()=>{
+//     console.log('hehheehe');
+    
+//     $('#btn1').fadeOut()
+// })
+
+// $('#btn2').on('click',()=>{
+//     console.log('hehheehe');
+    
+//     $('#btn1').fadeIn()
+// })
+
+
+// let arr = obj[{
+//     name: 'sai',
+//     age: 25
+// }]
+// console.log(arr);
+
+
+                                 // CONSTRUCTOR function//
+// function user (a,b){
+//     this.firstName = a,
+//     this.lastName = b
+// }
+// let user1 = new user('sai','kumar')
+// let user2 = new user('pratham','singh')
+
+// console.log(user1,user2);
+
+
+                               //IMPORT EXPORT//
+
+import {user,sum} from './home.js'
+console.log(user,'hehehh');
+sum(5,5)                               
